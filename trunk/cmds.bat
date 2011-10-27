3fff145190984d349e3a9e74757fc6de3e0281d7
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2011-10-27 15:25:13 +0000

    commands

--- trunk/cmds.bat
+++ trunk/cmds.bat
@@ -6,12 +6,13 @@ echo u. update code
 echo c. clean target
 echo p. create package
 echo s. build site
-echo d. deploy
-echo r. release
+echo d. deploy remotely
+echo r. release remotely
+echo i. install locally
 echo w. clear window
 echo q. quit
 echo ------------------
-CHOICE /N /C:ucpsdwrq
+CHOICE /N /C:ucpsdwriq
 set nr=%ERRORLEVEL%
 IF %nr% EQU 1 goto lupdate
 IF %nr% EQU 2 goto lclean
@@ -20,7 +21,8 @@ IF %nr% EQU 4 goto lsite
 IF %nr% EQU 5 goto ldeploy
 IF %nr% EQU 6 goto lcls
 IF %nr% EQU 7 goto lrelease
-IF %nr% EQU 8 goto end
+IF %nr% EQU 8 goto linstall
+IF %nr% EQU 9 goto end
 goto start
 
 :lupdate
