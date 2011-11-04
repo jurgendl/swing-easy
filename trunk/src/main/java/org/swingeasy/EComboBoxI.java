2e4835740c5baf7c32d569fd8f676833036be342
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2011-11-04 12:17:54 +0000

    fixed eventthreadsafe method ecombobox

---
+++ trunk/src/main/java/org/swingeasy/EComboBoxI.java
@@ -0,0 +1,5 @@
+package org.swingeasy;
+
+public interface EComboBoxI<T> {
+    public void activateAutoCompletion();
+}
--- trunk/src/test/java/org/swingeasy/ComboBoxDemo.java
+++ trunk/src/test/java/org/swingeasy/ComboBoxDemo.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 
 public class ComboBoxDemo {
     public static void main(String[] args) {
-        EComboBox<String> cc = new EComboBox<String>(new EComboBoxConfig(), "1", "2", "3");
+        EComboBox<String> cc = new EComboBox<String>(new EComboBoxConfig(), "111", "222", "333");
         JFrame f = new JFrame();
         f.getContentPane().add(cc, BorderLayout.CENTER);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
