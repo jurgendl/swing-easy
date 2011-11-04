bc71297894f885c6b5ff11078fa3f0d0a149a2d3
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2011-11-04 12:02:30 +0000

    demo's

---
+++ src/main/java/org/swingeasy/EComboBoxConfig.java
@@ -0,0 +1,5 @@
+package org.swingeasy;
+
+public class EComboBoxConfig {
+
+}
---
+++ src/test/java/org/swingeasy/ComboBoxDemo.java
@@ -0,0 +1,15 @@
+package org.swingeasy;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+
+public class ComboBoxDemo {
+    public static void main(String[] args) {
+        EComboBox<String> cc = new EComboBox<String>(new EComboBoxConfig(), "1", "2", "3");
+        JFrame f = new JFrame();
+        f.getContentPane().add(cc, BorderLayout.CENTER);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+}
