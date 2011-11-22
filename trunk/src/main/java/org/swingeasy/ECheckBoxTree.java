b6f7e06126b0b55f500d5373fda277596a786137
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2011-11-22 17:01:42 +0000

    (e)checkboxtree

---
+++ trunk/src/main/java/org/swingeasy/ECheckBoxTree.java
@@ -0,0 +1,35 @@
+package org.swingeasy;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTree;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreePath;
+
+/**
+ * @author Jurgen
+ */
+public class ECheckBoxTree<T> extends JTree {
+    private static final long serialVersionUID = 6378784816121886802L;
+
+    public ECheckBoxTree(ECheckBoxTreeNode<T> root) {
+        super(root);
+
+        this.setCellRenderer(new ECheckBoxTreeNodeRenderer());
+
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                TreePath path = ECheckBoxTree.this.getPathForLocation(e.getPoint().x, e.getPoint().y);
+                if (path != null) {
+                    @SuppressWarnings("unchecked")
+                    ECheckBoxTreeNode<T> node = ECheckBoxTreeNode.class.cast(path.getLastPathComponent());
+                    node.setSelected(!node.isSelected());
+                    DefaultTreeModel.class.cast(ECheckBoxTree.this.getModel()).nodeChanged(node);
+                }
+            }
+        });
+        this.setEditable(false);
+    }
+}
