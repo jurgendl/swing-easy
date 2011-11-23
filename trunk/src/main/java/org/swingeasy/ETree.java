5caf946bdf28f341ba1b8a66e5fb72a9a41d7640
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2011-11-23 16:25:36 +0000

    etree

--- trunk/src/main/java/org/swingeasy/ETree.java
+++ trunk/src/main/java/org/swingeasy/ETree.java
@@ -1,15 +1,22 @@
 package org.swingeasy;
 
 import javax.swing.JTree;
+import javax.swing.ToolTipManager;
+import javax.swing.tree.DefaultTreeModel;
 
 /**
  * @author Jurgen
  */
-public class ETree extends JTree implements ETreeI {
+public class ETree<T> extends JTree implements ETreeI<T> {
     private static final long serialVersionUID = -2866936668266217327L;
 
-    public ETree() {
-        super();
+    public ETree(ETreeNode<T> rootNode) {
+        super(new DefaultTreeModel(rootNode));
+        this.setShowsRootHandles(true);
+        this.setRootVisible(true);
+        this.setRowHeight(17);
+        this.setCellRenderer(new ETreeNodeRenderer());
+        ToolTipManager.sharedInstance().registerComponent(this);
     }
 
     /**
@@ -17,21 +24,22 @@ public class ETree extends JTree implements ETreeI {
      * 
      * @return
      */
-    public ETreeI getSimpleThreadSafeInterface() {
+    @SuppressWarnings("unchecked")
+    public ETreeI<T> getSimpleThreadSafeInterface() {
         return EventThreadSafeWrapper.getSimpleThreadSafeInterface(ETree.class, this, ETreeI.class);
     }
 
     /**
      * @see #getSimpleThreadSafeInterface()
      */
-    public ETreeI stsi() {
+    public ETreeI<T> stsi() {
         return this.getSimpleThreadSafeInterface();
     }
 
     /**
      * @see #getSimpleThreadSafeInterface()
      */
-    public ETreeI STSI() {
+    public ETreeI<T> STSI() {
         return this.getSimpleThreadSafeInterface();
     }
 }
