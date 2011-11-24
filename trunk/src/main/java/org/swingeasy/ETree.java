8e906d5426664167286f1fe8f0ac4fd35fe08256
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2011-11-24 13:21:04 +0000

    etree

--- trunk/src/main/java/org/swingeasy/ETree.java
+++ trunk/src/main/java/org/swingeasy/ETree.java
@@ -2,21 +2,24 @@ package org.swingeasy;
 
 import javax.swing.JTree;
 import javax.swing.ToolTipManager;
-import javax.swing.tree.DefaultTreeModel;
 
 /**
  * @author Jurgen
  */
 public class ETree<T> extends JTree implements ETreeI<T> {
+
     private static final long serialVersionUID = -2866936668266217327L;
 
     public ETree(ETreeNode<T> rootNode) {
-        super(new DefaultTreeModel(rootNode));
+        super(new javax.swing.tree.DefaultTreeModel(rootNode, true));
         this.setShowsRootHandles(true);
         this.setRootVisible(true);
-        this.setRowHeight(17);
+
         this.setCellRenderer(new ETreeNodeRenderer());
         ToolTipManager.sharedInstance().registerComponent(this);
+
+        this.setEditable(true);
+        this.setCellEditor(new ETreeNodeEditor());
     }
 
     /**
