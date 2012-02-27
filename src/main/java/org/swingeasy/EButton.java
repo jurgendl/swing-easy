1ed76c082f4ed04f72e911d21845baf21e82fa12
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2012-02-27 11:03:23 +0000

    renamed etoolbarbutton to ebutton

--- src/main/java/org/swingeasy/EButton.java
+++ src/main/java/org/swingeasy/EButton.java
@@ -7,20 +7,20 @@ import javax.swing.JButton;
 /**
  * @author Jurgen
  */
-public class EToolBarButton extends JButton implements EComponentI {
+public class EButton extends JButton implements EComponentI {
     private static final long serialVersionUID = -6193067407274776197L;
 
-    public EToolBarButton(EButtonCustomizer ebc) {
+    public EButton(EButtonCustomizer ebc) {
         super();
         ebc.customize(this);
     }
 
-    public EToolBarButton(EButtonCustomizer ebc, Action a) {
+    public EButton(EButtonCustomizer ebc, Action a) {
         super(a);
         ebc.customize(this);
     }
 
-    public EToolBarButton(EButtonCustomizer ebc, Icon icon) {
+    public EButton(EButtonCustomizer ebc, Icon icon) {
         super(icon);
         ebc.customize(this);
     }
