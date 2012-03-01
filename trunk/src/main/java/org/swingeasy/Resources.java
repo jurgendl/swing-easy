31ee50744c4a515c63157af6d3ed65830532c9fa
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2012-03-01 16:54:49 +0000

    ETextArea utility methods default popup menu for text components new: SearchDialog extra methods to get resources

--- trunk/src/main/java/org/swingeasy/Resources.java
+++ trunk/src/main/java/org/swingeasy/Resources.java
@@ -1,7 +1,11 @@
 package org.swingeasy;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 
@@ -38,5 +42,46 @@ public class Resources {
 
     public static ImageIcon getImageResource(String key) {
         return Resources.getImageResource(Resources.getDefaultClassLoader(), key);
+    }
+
+    public static byte[] getResource(ClassLoader cl, String key) throws IOException {
+        File file = new File(key);
+        if (file.exists()) {
+            return Resources.read(file);
+        }
+        URL url = cl.getResource(key);
+        if (url != null) {
+            return Resources.read(url);
+        }
+        throw new IOException("resource not found: " + key);
+    }
+
+    public static byte[] getResource(String key) throws IOException {
+        return Resources.getResource(Resources.getDefaultClassLoader(), key);
+    }
+
+    public static byte[] read(File file) throws IOException {
+        InputStream in = new FileInputStream(file);
+        byte[] data = Resources.read(in);
+        in.close();
+        return data;
+    }
+
+    public static byte[] read(InputStream in) throws IOException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream(in.available());
+        byte[] buffer = new byte[1024 * 8];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+        out.close();
+        return out.toByteArray();
+    }
+
+    public static byte[] read(URL url) throws IOException {
+        InputStream in = url.openStream();
+        byte[] data = Resources.read(in);
+        in.close();
+        return data;
     }
 }
