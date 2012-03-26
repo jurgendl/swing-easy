50f00fdfaf774bb69ae048bb3bf4bfd8b19beebe
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2012-03-26 14:50:45 +0000

    [maven-release-plugin]  copy for tag swing-easy-2.5.2

---
+++ tags/swing-easy-2.5.2/src/main/java/org/swingeasy/Messages.java
@@ -0,0 +1,45 @@
+package org.swingeasy;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+import org.swingeasy.system.SystemSettings;
+
+/**
+ * @author Jurgen
+ */
+public class Messages {
+    private static final String BUNDLE_NAME = "org.swingeasy.resources.swing-easy"; //$NON-NLS-1$
+
+    private static final Map<Locale, ResourceBundle> RESOURCE_BUNDLES = new HashMap<Locale, ResourceBundle>();
+
+    private static ResourceBundle getResourceBundle(Locale locale) {
+        if (locale == null) {
+            locale = SystemSettings.getCurrentLocale();
+        }
+        ResourceBundle resourceBundle = Messages.RESOURCE_BUNDLES.get(locale);
+        if (resourceBundle == null) {
+            resourceBundle = ResourceBundle.getBundle(Messages.BUNDLE_NAME, locale);
+        }
+        return resourceBundle;
+    }
+
+    public static String getString(Locale locale, String key) {
+        if (locale == null) {
+            locale = SystemSettings.getCurrentLocale();
+        }
+        try {
+            return Messages.getResourceBundle(locale).getString(key);
+        } catch (MissingResourceException e) {
+            System.out.println(key);
+            return key;
+        }
+    }
+
+    private Messages() {
+        super();
+    }
+}
