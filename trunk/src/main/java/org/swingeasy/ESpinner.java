ded978e2d3bd7e1244fc6697aeff4d8c9ee67365
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2012-03-29 14:54:07 +0000

    tooltips for everybody new: ERadioButton

--- trunk/src/main/java/org/swingeasy/ESpinner.java
+++ trunk/src/main/java/org/swingeasy/ESpinner.java
@@ -5,6 +5,7 @@ import java.awt.event.MouseWheelListener;
 
 import javax.swing.JSpinner;
 import javax.swing.SpinnerModel;
+import javax.swing.ToolTipManager;
 
 /**
  * @author Jurgen
@@ -14,22 +15,34 @@ public class ESpinner<T> extends JSpinner {
 
     public ESpinner(SpinnerModel model) {
         super(model);
-
-        this.addMouseWheelListener(new MouseWheelListener() {
-            @Override
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                if (e.getWheelRotation() < 0) {
-                    ESpinner.this.gotoNextValue();
-                } else {
-                    ESpinner.this.gotoPreviousValue();
-                }
-            }
-        });
+        this.init();
     }
 
     @SuppressWarnings("unchecked")
     public T get() {
         return (T) this.getValue();
+    }
+
+    /**
+     * 
+     * @see javax.swing.JComponent#getToolTipText()
+     */
+    @Override
+    public String getToolTipText() {
+        String toolTipText = super.getToolTipText();
+        if (toolTipText == null) {
+            // FIXME
+            // Object value = this.getValue();
+            // if (value == null) {
+            // return null;
+            // }
+            // String text = String.valueOf(value);
+            // if (text.trim().length() == 0) {
+            // text = null;
+            // }
+            // return text;
+        }
+        return toolTipText;
     }
 
     public T gotoNextValue() {
@@ -44,5 +57,20 @@ public class ESpinner<T> extends JSpinner {
         T nextValue = (T) ESpinner.this.getPreviousValue();
         ESpinner.this.getModel().setValue(nextValue);
         return nextValue;
+    }
+
+    protected void init() {
+        this.addMouseWheelListener(new MouseWheelListener() {
+            @Override
+            public void mouseWheelMoved(MouseWheelEvent e) {
+                if (e.getWheelRotation() < 0) {
+                    ESpinner.this.gotoNextValue();
+                } else {
+                    ESpinner.this.gotoPreviousValue();
+                }
+            }
+        });
+
+        ToolTipManager.sharedInstance().registerComponent(this);
     }
 }
