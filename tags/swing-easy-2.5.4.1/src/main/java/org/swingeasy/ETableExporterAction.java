d45c78b6003bbebe72e9d4bc858a8268fda37db1
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2012-04-17 09:50:39 +0000

    [maven-release-plugin]  copy for tag swing-easy-2.5.4.1

---
+++ tags/swing-easy-2.5.4.1/src/main/java/org/swingeasy/ETableExporterAction.java
@@ -0,0 +1,29 @@
+package org.swingeasy;
+
+import java.awt.event.ActionEvent;
+
+import org.swingeasy.EComponentPopupMenu.EComponentPopupMenuAction;
+
+/**
+ * @author Jurgen
+ */
+public class ETableExporterAction<T> extends EComponentPopupMenuAction<ETable<T>> {
+    
+    private static final long serialVersionUID = -4509106311973499954L;
+
+    protected final ETableExporter<T> exporter;
+
+    public ETableExporterAction(ETableExporter<T> exporter, ETable<T> table) {
+        super(table, exporter.getAction(), exporter.getIcon());
+        this.exporter = exporter;
+    }
+
+    /**
+     * 
+     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+     */
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.exporter.export(this.delegate);
+    }
+}
---
+++ tags/swing-easy-2.5.4.1/src/main/java/org/swingeasy/ETablePdfExporter.java
@@ -0,0 +1,71 @@
+package org.swingeasy;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xhtmlrenderer.pdf.ITextRenderer;
+import org.xml.sax.SAXException;
+
+import com.lowagie.text.DocumentException;
+
+/**
+ * @author Jurgen
+ */
+public class ETablePdfExporter<T> extends ETableHtmlExporter<T> {
+    /**
+     * 
+     * @see org.swingeasy.ETableHtmlExporter#exportStream(org.swingeasy.ETable)
+     */
+    @Override
+    public InputStream exportStream(ETable<T> table) throws IOException {
+        try {
+            String data = this.createHtml(table);
+            String css = new String(Resources.getDataResource("exporter/css.txt"));
+            data = data.replace("<head>", "<head><style type=\"text/css\">" + css + "</style>");
+
+            ITextRenderer itextRenderer = new ITextRenderer();
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+
+            org.w3c.dom.Document doc = builder.parse(new ByteArrayInputStream(data.getBytes("UTF-8")));
+            itextRenderer.setDocument(doc, null);
+
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            itextRenderer.layout();
+            itextRenderer.createPDF(os);
+            os.close();
+
+            return new ByteArrayInputStream(os.toByteArray());
+        } catch (ParserConfigurationException ex) {
+            throw new IOException(ex);
+        } catch (DocumentException ex) {
+            throw new IOException(ex);
+        } catch (SAXException ex) {
+            throw new IOException(ex);
+        }
+    }
+
+    /**
+     * 
+     * @see org.swingeasy.ETableExporter#getAction()
+     */
+    @Override
+    public String getAction() {
+        return "pdf-export";
+    }
+
+    /**
+     * 
+     * @see org.swingeasy.ETableExporter#getFileExtension()
+     */
+    @Override
+    public String getFileExtension() {
+        return "pdf";
+    }
+}
