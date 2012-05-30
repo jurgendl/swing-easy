d91aa5105a66f149b727c8d04b076c2831b750f5
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2012-05-30 15:12:20 +0000

    demo

--- src/test/java/org/swingeasy/FormDemo.java
+++ src/test/java/org/swingeasy/FormDemo.java
@@ -1,8 +1,10 @@
 package org.swingeasy;
 
 import java.awt.Container;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
 
@@ -14,7 +16,7 @@ import org.swingeasy.form.FormBuilder;
 public class FormDemo {
     private static void addComponents(Container container) {
         FormBuilder builder = new FormBuilder(2);
-        builder.setDebug(false);
+        builder.setDebug(true);
 
         builder.addTitle("Somewhat longer than label - title 1", 2);
         builder.addComponent("Label1", new ETextField(new ETextFieldConfig()), null);
@@ -29,10 +31,26 @@ public class FormDemo {
         builder.addComponent("Label5", new ETextField(new ETextFieldConfig()), null);
         builder.addComponent("Label6", new ETextField(new ETextFieldConfig()), null);
 
+        EComboBoxConfig cfg = new EComboBoxConfig();
+        EComboBox<String> component = new EComboBox<String>(cfg).stsi();
+        component
+                .addRecord(new EComboBoxRecord<String>(
+                        "aaaaaaaaazzzzzzzzzzeeeeeeeeerrrrrrrrrtttttttttttttyyyyyyyyyyyy111111111222222223333333334444444444455555555566666666777777788888888889999999"));
+        Dimension preferredSize = component.getPreferredSize();
+        System.out.println(preferredSize);
+        component.setPreferredSize(new Dimension(222, preferredSize.height));
+        component.setSize(new Dimension(222, preferredSize.height));
+        component.setMaximumSize(new Dimension(222, preferredSize.height));
+        builder.addComponent("combo", component.getParentComponent(), null);
+
+        builder.addComponent("", new JLabel(), null);
+
         container.add(builder.getContainer());
     }
 
     public static void main(String[] args) {
+        UIUtils.systemLookAndFeel();
+
         JFrame frame = new JFrame();
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         FormDemo.addComponents(frame.getContentPane());
