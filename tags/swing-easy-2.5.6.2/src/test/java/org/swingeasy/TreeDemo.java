6889b90c4bb2c96e1f1eb05b895a2806615ea8b0
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2013-06-03 12:22:59 +0000

    [maven-release-plugin]  copy for tag swing-easy-2.5.6.2

---
+++ tags/swing-easy-2.5.6.2/src/test/java/org/swingeasy/TreeDemo.java
@@ -0,0 +1,57 @@
+package org.swingeasy;
+
+import java.awt.BorderLayout;
+import java.util.List;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import javax.swing.WindowConstants;
+
+/**
+ * @author Jurgen
+ */
+public class TreeDemo {
+    private static class DummyNode extends ETreeNode<String> {
+        private static final long serialVersionUID = 4389106694997553842L;
+
+        public DummyNode() {
+            this("0"); //$NON-NLS-1$
+        }
+
+        public DummyNode(String userObject) {
+            super(userObject);
+        }
+
+        @Override
+        protected void initChildren(List<ETreeNode<String>> list) {
+            System.out.println("lazy-init " + this); //$NON-NLS-1$
+            if (!this.getUserObject().toString().endsWith("5")) { //$NON-NLS-1$
+                for (int i = 0; i < 10; i++) {
+                    String s = this.getUserObject() + "." + i; //$NON-NLS-1$
+                    list.add(new DummyNode(s));
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIUtils.systemLookAndFeel();
+            final JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            ETree<String> tree = new ETree<String>(new DummyNode());
+            frame.getContentPane().add(new JScrollPane(tree), BorderLayout.CENTER);
+            frame.getContentPane().add(tree.getSearchComponent(), BorderLayout.NORTH);
+            SwingUtilities.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    frame.setSize(200, 200);
+                    frame.setVisible(true);
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
