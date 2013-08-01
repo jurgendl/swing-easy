82120d47abb09e50a2c56e554ac40253b7f5a935
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2013-08-01 10:54:17 +0000

    less optional libraries (javassist is required)

--- src/main/java/org/swingeasy/EventThreadSafeWrapper.java
+++ src/main/java/org/swingeasy/EventThreadSafeWrapper.java
@@ -4,41 +4,12 @@ package org.swingeasy;
  * @author Jurgen
  */
 public class EventThreadSafeWrapper<C> {
-    public static enum WrapperType {
-        JAVASSIST, CGLIB, NONE;
-    }
-
-    private static WrapperType type = WrapperType.JAVASSIST;
-
     public static <C, I> C getSimpleThreadSafeInterface(final Class<C> componentClass, final C component, final Class<I> interfaced) {
-        switch (EventThreadSafeWrapper.type) {
-            case JAVASSIST:
-                try {
-                    return org.swingeasy.JavassistEventThreadSafeWrapper.getSimpleThreadSafeInterface(componentClass, component, interfaced);
-                } catch (Throwable ex) {
-                    ex.printStackTrace();
-                    EventThreadSafeWrapper.type = WrapperType.NONE;
-                    return component;
-                }
-            case CGLIB:
-                try {
-                    return org.swingeasy.CglibEventThreadSafeWrapper.getSimpleThreadSafeInterface(componentClass, component, interfaced);
-                } catch (Throwable ex) {
-                    ex.printStackTrace();
-                    EventThreadSafeWrapper.type = WrapperType.NONE;
-                    return component;
-                }
-            case NONE:
-                return component;
+        try {
+            return org.swingeasy.JavassistEventThreadSafeWrapper.getSimpleThreadSafeInterface(componentClass, component, interfaced);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+            return component;
         }
-        return component;
-    }
-
-    public static WrapperType getType() {
-        return EventThreadSafeWrapper.type;
-    }
-
-    public static void setType(WrapperType type) {
-        EventThreadSafeWrapper.type = type;
     }
 }
