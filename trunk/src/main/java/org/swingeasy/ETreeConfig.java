458cca4c606da55fc8b15d543c4770d58b2d2387
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2013-09-11 12:08:31 +0000

    etree editable in config

--- trunk/src/main/java/org/swingeasy/ETreeConfig.java
+++ trunk/src/main/java/org/swingeasy/ETreeConfig.java
@@ -4,5 +4,19 @@ package org.swingeasy;
  * @author Jurgen
  */
 public class ETreeConfig extends EComponentConfig<ETreeConfig> {
-    //
+    private boolean editable = true;
+
+    public ETreeConfig() {
+        super();
+    }
+
+    public boolean isEditable() {
+        return this.editable;
+    }
+
+    public ETreeConfig setEditable(boolean editable) {
+        this.lockCheck();
+        this.editable = editable;
+        return this;
+    }
 }
