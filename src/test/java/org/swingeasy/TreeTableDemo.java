3d570c76d3d5c02a6c8532e0abc6f923750c9306
jurgen.de.landsheer@gmail.com <jurgen.de.landsheer@gmail.com@fcfd4c22-4719-41e4-afb2-e5e335fe1975>
2013-09-20 15:29:23 +0000

    ETreeTable reworked

--- src/test/java/org/swingeasy/TreeTableDemo.java
+++ src/test/java/org/swingeasy/TreeTableDemo.java
@@ -2,6 +2,7 @@ package org.swingeasy;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
@@ -15,19 +16,19 @@ public class TreeTableDemo {
 
         String value;
 
-        String tmp;
+        Date tmp;
 
-        public Rec(String key, String value, String tmp) {
+        public Rec(String key, String value, int i) {
             this.key = key;
             this.value = value;
-            this.tmp = tmp;
+            this.tmp = new Date(new Date().getTime() + (i * 10000000l));
         }
 
         public String getKey() {
             return this.key;
         }
 
-        public String getTmp() {
+        public Date getTmp() {
             return this.tmp;
         }
 
@@ -39,7 +40,7 @@ public class TreeTableDemo {
             this.key = key;
         }
 
-        public void setTmp(String tmp) {
+        public void setTmp(Date tmp) {
             this.tmp = tmp;
         }
 
@@ -63,22 +64,25 @@ public class TreeTableDemo {
 
         Collection<ETreeTableRecord<Rec>> coll = new ArrayList<ETreeTableRecord<Rec>>();
         for (int i = 0; i < 10; i++) {
-            Rec lvl1 = new Rec("level 0 : " + i, "description level 0 : " + i, "" + (i * 100));
+            Rec lvl1 = new Rec("level 0 : " + i, "description level 0 : " + i, (i * 100));
             ETreeTableRecordBean<Rec> rec1 = new ETreeTableRecordBean<Rec>(null, lvl1, h);
             coll.add(rec1);
             for (int j = 0; j < 10; j++) {
-                Rec lvl2 = new Rec("level 1 : " + j, "description level 1 : " + i, "" + ((i * 100) + (j * 10)));
+                Rec lvl2 = new Rec("level 1 : " + j, "description level 1 : " + i, ((i * 100) + (j * 10)));
                 ETreeTableRecordBean<Rec> rec2 = new ETreeTableRecordBean<Rec>(rec1, lvl2, h);
                 coll.add(rec2);
                 for (int k = 0; k < 10; k++) {
-                    Rec lvl3 = new Rec("level 2 : " + k, "value " + ((i * 100) + (j * 10) + k), "" + ((i * 100) + (j * 10) + k));
+                    Rec lvl3 = new Rec("level 2 : " + k, "value " + ((i * 100) + (j * 10) + k), ((i * 100) + (j * 10) + k));
                     ETreeTableRecordBean<Rec> rec3 = new ETreeTableRecordBean<Rec>(rec2, lvl3, h);
                     coll.add(rec3);
                 }
             }
         }
 
-        ETreeTableHeaders<Rec> headers = new ETreeTableHeaders<Rec>(h);
+        ETreeTableHeaders<Rec> headers = new ETreeTableHeaders<Rec>();
+        headers.add(h[0]);
+        headers.add(h[1]);
+        headers.add(h[2], Date.class);
         ETreeTable<Rec> t = new ETreeTable<Rec>(new ETreeTableConfig(), headers);
         t.stsi().addRecords(coll);
         f.getContentPane().add(new JScrollPane(t));
